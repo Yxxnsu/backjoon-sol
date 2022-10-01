f7bec4ccf8739f2642f466954999c5cc0d79f1e3
Yxxnsu_ <64016453+Yxxnsu@users.noreply.github.com>
2022-10-01 14:39:57 +0900

    [Silver V] Title: 거스름돈, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/14916. 거스름돈/거스름돈.py
+++ 백준/Silver/14916. 거스름돈/거스름돈.py
@@ -4,21 +4,18 @@ cnt = 0
 if n % 5 == 0:
 	print(n // 5)	
 else:
-	if (n % 5) % 2 == 0:
-		print((n // 5) + ((n % 5) // 2))
-	else:
-		result = []
-		i = 0
-		while True:
-			cnt = i
-			if n < 0: 
-				break
-			if n % 2 == 0:
-				cnt += (n // 2)
-				result.append(cnt)
-			n -= 5
-			i += 1
-		print(min(result)) if len(result) else print(-1)
+	result = []
+	i = 0
+	while True:
+		cnt = i
+		if n < 0: 
+			break
+		if n % 2 == 0:
+			cnt += (n // 2)
+			result.append(cnt)
+		n -= 5
+		i += 1
+	print(min(result)) if len(result) else print(-1)
 			
 	
 	
