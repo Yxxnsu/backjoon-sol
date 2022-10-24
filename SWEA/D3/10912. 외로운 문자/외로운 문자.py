23123222214d44b7f5634a1ff2c398072289ab94
Yxxnsu_ <64016453+Yxxnsu@users.noreply.github.com>
2022-10-24 21:26:32 +0900

    [D3] Title: 외로운 문자, Time: 318 ms, Memory: 60,244 KB -BaekjoonHub

--- SWEA/D3/10912. 외로운 문자/외로운 문자.py
+++ SWEA/D3/10912. 외로운 문자/외로운 문자.py
@@ -7,21 +7,9 @@ def sol():
 		n_s = ''.join(sorted(s))
 		return n_s
 	else:
-		ans = []
-		for v in s:
-			if v in ans:
-				continue
-			else:
-				v_cnt = s.count(v)
-				if v_cnt >= 2:
-					if v_cnt % 2 != 0:
-						ans.append(v)
-				else:
-					ans.append(v)
-		if len(ans) == 0:
-			return 'Good'
-		else:
-			ans.sort()
-			return ''.join(ans)
+		ans = [v for v in s if s.count(v) % 2 == 1]
+		ans = list(set(ans))
+		return ''.join(sorted(ans)) if len(ans) != 0 else 'Good'
+		
 for tc in range(1, T+1):
 	print(f'#{tc} {sol()}')
