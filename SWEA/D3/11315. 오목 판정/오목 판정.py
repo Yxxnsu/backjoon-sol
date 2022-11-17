2dfa5fff191f551df4df3735bf1d5f13085dd638
Yxxnsu_ <64016453+Yxxnsu@users.noreply.github.com>
2022-11-17 20:17:13 +0900

    [D3] Title: 오목 판정, Time: 198 ms, Memory: 62,764 KB -BaekjoonHub

--- SWEA/D3/11315. 오목 판정/오목 판정.py
+++ SWEA/D3/11315. 오목 판정/오목 판정.py
@@ -6,14 +6,14 @@ def sol():
 
     matrix = [list(input()) for _ in range(N)]
 
-    dx = [-1,1,0,0,-1,-1,1,1]
-    dy = [0,0,-1,1,-1,1,-1,1]
+    dx = [1,0,1,1]
+    dy = [0,1,-1,1]
 
 
     for i in range(N):
         for j in range(N):
             if matrix[i][j] == 'o':
-                for k in range(8):
+                for k in range(4):
                     nx = i + dx[k]
                     ny = j + dy[k]
                     
