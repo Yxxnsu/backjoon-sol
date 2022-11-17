69a32b146be5dc57718f2a11b87c8bf84947ba27
Yxxnsu_ <64016453+Yxxnsu@users.noreply.github.com>
2022-11-17 17:41:55 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문1, Time: 166 ms, Memory: 59,872 KB -BaekjoonHub

---
+++ SWEA/D3/1228. ［S／W 문제해결 기본］ 8일차 － 암호문1/［S／W 문제해결 기본］ 8일차 － 암호문1.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+T = 10
+
+def sol():
+
+    N = int(input())
+    li = list(input().split())
+    K = int(input())
+    command = list(input().split())
+
+    for i, v in enumerate(command):
+        if v == 'I':
+            start = i + 3
+            end = start + int(command[i+2])
+            s = command[start:end]
+
+            for k in s[::-1]:
+                li.insert(int(command[i+1]), k)
+    
+    return ' '.join(li[:10])
+
+for tc in range(1, T+1):
+    print(f'#{tc} {sol()}')
