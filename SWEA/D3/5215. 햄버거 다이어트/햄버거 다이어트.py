ceb931734701f60f5724eb79d92c2d64890561ac
Yxxnsu_ <64016453+Yxxnsu@users.noreply.github.com>
2022-11-18 21:27:21 +0900

    [D3] Title: 햄버거 다이어트, Time: 4,652 ms, Memory: 72,804 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.py
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.py
@@ -1,30 +1,31 @@
 T = int(input())
 
-def sol():
+def DFS(i):
+    global ans
+     
+    if i == N:
+        taste, kcal = 0, 0
+        for i in range(N):
+            if visited[i]:
+                t, k = li[i]
+                taste += t
+                kcal += k
+        if kcal > L:
+            return
+        ans = max(ans, taste)
+        return 
+
+    visited[i] = True
+    DFS(i+1)
+    visited[i] = False
+    DFS(i+1)
     
+for tc in range(1, T+1):
     N, L = map(int, input().split())
     li = [list(map(int, input().split())) for _ in range(N)]
 
-    def dfs(idx, c_taste, c_kcal):
-        global max_tasty
-        if c_kcal > L:
-            return
-        
-        if max_tasty < c_taste:
-            max_tasty = c_taste
-        
-        if idx == N:
-            return 
+    ans = 0
+    visited = [False] * N
 
-        taste, kcal = li[idx]
-
-        dfs(idx + 1, c_taste + taste, c_kcal + kcal)
-        dfs(idx + 1, c_taste, c_kcal)
-    
-    dfs(0, 0, 0)
-
-    return max_tasty
-
-for tc in range(1, T+1):
-    max_tasty = 0
-    print(f'#{tc} {sol()}')
+    DFS(0)
+    print(f'#{tc} {ans}')
