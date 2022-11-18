308c7f19718153d49bbda1aaa90dd17d1a36be29
Yxxnsu_ <64016453+Yxxnsu@users.noreply.github.com>
2022-11-18 21:32:09 +0900

    [D3] Title: 햄버거 다이어트, Time: 643 ms, Memory: 65,424 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.py
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.py
@@ -1,31 +1,26 @@
 T = int(input())
 
-def DFS(i):
+def DFS(i, taste, kcal):
     global ans
-     
-    if i == N:
-        taste, kcal = 0, 0
-        for i in range(N):
-            if visited[i]:
-                t, k = li[i]
-                taste += t
-                kcal += k
-        if kcal > L:
-            return
-        ans = max(ans, taste)
-        return 
 
-    visited[i] = True
-    DFS(i+1)
-    visited[i] = False
-    DFS(i+1)
+    if kcal > L:
+        return
     
+    ans = max(ans, taste)
+    
+    if i == N:
+        return 
+    
+    t, k = li[i]
+
+    DFS(i+1, taste, kcal)
+    DFS(i+1, taste + t, kcal + k)
+     
 for tc in range(1, T+1):
     N, L = map(int, input().split())
     li = [list(map(int, input().split())) for _ in range(N)]
 
-    ans = 0
-    visited = [False] * N
-
-    DFS(0)
+    ans = 0    
+    DFS(0, 0, 0)
+    
     print(f'#{tc} {ans}')
