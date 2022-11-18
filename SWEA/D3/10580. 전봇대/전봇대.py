272eaa3b65d06d0d971c306861fbcbd322049752
Yxxnsu_ <64016453+Yxxnsu@users.noreply.github.com>
2022-11-18 12:23:20 +0900

    [D3] Title: 전봇대, Time: 520 ms, Memory: 66,220 KB -BaekjoonHub

--- SWEA/D3/10580. 전봇대/전봇대.py
+++ SWEA/D3/10580. 전봇대/전봇대.py
@@ -1,18 +1,18 @@
 T = int(input())
- 
+
 def sol():
 
-    n = int(input())
-    li = [list(map(int, input().split())) for _ in range(n)]
+    N = int(input())
+    li = [list(map(int, input().split())) for _ in range(N)]
 
     ans = 0
-    for i in range(n):
-       for j in range(i+1, n):
-            ia, ib = li[i]
-            ja, jb = li[j]
-            if (ia > ja and ib < jb) or (ja > ia and jb < ib):
+    for i in range(N):
+        for j in range(i, N):
+            ai, bi = li[i]
+            aj, bj = li[j]
+            if (ai < aj and bi > bj) or (ai > aj and bi < bj):
                 ans += 1
     return ans
-
+    
 for tc in range(1, T+1):
     print(f'#{tc} {sol()}')
