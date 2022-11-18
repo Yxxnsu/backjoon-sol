4040e548ef8d5f1ac0486ecedd6e9751fa90548f
Yxxnsu_ <64016453+Yxxnsu@users.noreply.github.com>
2022-11-19 00:30:12 +0900

    [D3] Title: 늘어지는 소리 만들기, Time: 402 ms, Memory: 63,868 KB -BaekjoonHub

--- SWEA/D3/4676. 늘어지는 소리 만들기/늘어지는 소리 만들기.py
+++ SWEA/D3/4676. 늘어지는 소리 만들기/늘어지는 소리 만들기.py
@@ -2,18 +2,16 @@ T = int(input())
 
 def sol():
 
-    s = list(input())
-    h = int(input())
-    pos = list(map(int, input().split()))
+    word = list(input())
 
-    ans = ''
-    for i in range(len(s)+1):
-        if i == len(s):
-            ans += '-' * (pos.count(i))
-        else:
-            ans += '-' * (pos.count(i))
-            ans += s[i]
-    return ans
+    H = int(input())
+    li = list(map(int, input().split()))
+    li.sort(reverse=True)
 
+    for v in li:
+        word.insert(v, '-')
+    
+    return ''.join(word)
+    
 for tc in range(1, T+1):
     print(f'#{tc} {sol()}')
