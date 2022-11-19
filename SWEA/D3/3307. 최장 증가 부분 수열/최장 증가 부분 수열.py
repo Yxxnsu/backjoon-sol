b520ff085b90be46517f57925ce8fa72fe9ec63c
Yxxnsu_ <64016453+Yxxnsu@users.noreply.github.com>
2022-11-19 20:33:05 +0900

    [D3] Title: 최장 증가 부분 수열, Time: 196 ms, Memory: 62,780 KB -BaekjoonHub

--- SWEA/D3/3307. 최장 증가 부분 수열/최장 증가 부분 수열.py
+++ SWEA/D3/3307. 최장 증가 부분 수열/최장 증가 부분 수열.py
@@ -4,15 +4,16 @@ def sol():
 
     N = int(input())
     li = list(map(int, input().split()))
-
+    
     dp = [0] * N
     dp[0] = 1
 
     for i in range(1, N):
-        for j in range(i-1, -1, -1):
+        for j in range(i-1,-1,-1):
             if li[i] > li[j]:
                 dp[i] = max(dp[i], dp[j])
         dp[i] += 1
+
     return max(dp)
 
 for tc in range(1, T+1):
