29b53835e8def43bc231920f51a128bed635eb06
Yxxnsu_ <64016453+Yxxnsu@users.noreply.github.com>
2022-11-19 22:54:24 +0900

    [D3] Title: 최장 공통 부분 수열, Time: 326 ms, Memory: 92,352 KB -BaekjoonHub

--- SWEA/D3/3304. 최장 공통 부분 수열/최장 공통 부분 수열.py
+++ SWEA/D3/3304. 최장 공통 부분 수열/최장 공통 부분 수열.py
@@ -1,15 +1,22 @@
-t = int(input())
+T = int(input())
 
-for tc in range(1, t + 1) :
-    a, b = input().split()
-    dp = [[0] * (len(a) + 1) for _ in range(len(b) + 1)]
+def LCS(x, y):
+    x, y = ' ' + x, ' ' + y
+    m, n = len(x), len(y)
 
-    for i in range(1, len(dp)) :
-        for j in range(1, len(dp[i])) :
-            # 같을 경우
-            if a[j-1] == b[i-1] :
+    dp = [[0] * n for _ in range(m)]
+
+    for i in range(1, m):
+        for j in range(1, n):
+            if x[i] == y[j]:
                 dp[i][j] = dp[i-1][j-1] + 1
-            else : # 다를 경우
+            else:
                 dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+    return dp
 
-    print('#%d %d' % (tc, dp[-1][-1]))
+for tc in range(1, T+1):
+    a, b = input().split()
+
+    ans = LCS(a, b)
+    
+    print(f'#{tc} {ans[-1][-1]}')
