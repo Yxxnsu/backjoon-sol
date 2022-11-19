074743ccea6dd291ce1c6d93813f9b90dc3065fe
Yxxnsu_ <64016453+Yxxnsu@users.noreply.github.com>
2022-11-19 12:25:26 +0900

    [D3] Title: 세가지 합 구하기, Time: 218 ms, Memory: 64,336 KB -BaekjoonHub

--- SWEA/D3/3408. 세가지 합 구하기/세가지 합 구하기.py
+++ SWEA/D3/3408. 세가지 합 구하기/세가지 합 구하기.py
@@ -9,6 +9,8 @@ def sol():
     s3 = s2 + N
 
     return f'{s1} {s2} {s3}'
-        
+
+ans = []   
 for tc in range(1, T+1):
-    print(f'#{tc} {sol()}')
+    ans.append(f'#{tc} {sol()}')
+print(*ans, sep='\n')
