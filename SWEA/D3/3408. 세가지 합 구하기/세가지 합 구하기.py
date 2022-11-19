e7d282110f0a7d1bf68ae5dbe8045183b1eb6210
Yxxnsu_ <64016453+Yxxnsu@users.noreply.github.com>
2022-11-19 12:24:44 +0900

    [D3] Title: 세가지 합 구하기, Time: 1,214 ms, Memory: 65,136 KB -BaekjoonHub

--- SWEA/D3/3408. 세가지 합 구하기/세가지 합 구하기.py
+++ SWEA/D3/3408. 세가지 합 구하기/세가지 합 구하기.py
@@ -1,13 +1,14 @@
 T = int(input())
 
 def sol():
-    
-    N = int(input())
-    s1 = (N + 1) * N // 2
-    s2 = 2 * s1 - N
-    return f'{s1} {s2} {s2 + N}'
 
-ans = []
+    N = int(input())
+
+    s1 = ((N+1) * N) // 2
+    s2 = s1 + ((N-1) * N) // 2
+    s3 = s2 + N
+
+    return f'{s1} {s2} {s3}'
+        
 for tc in range(1, T+1):
-    ans.append(f'#{tc} {sol()}')
-print(*ans, sep='\n')
+    print(f'#{tc} {sol()}')
